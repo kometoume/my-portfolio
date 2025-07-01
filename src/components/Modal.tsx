515f15b490cb566ae68d32c4d6e682484a462e98
Michie Yagi <kometoume@gmail.com>
2025-07-01 21:37:44 +0900

    モバイル修正文字色05

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -33,7 +33,7 @@ export default function Modal({ isOpen, onClose, children }: ModalProps) {
 
   return (
     <div
-      className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4"
+      className="fixed inset-0 bg-white bg-opacity-50 flex items-center justify-center z-50 p-4"
       onClick={handleOverlayClick}
     >
       <div
