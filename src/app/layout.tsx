ea370a67692c41587ec92133708ecced22343420
Michie Yagi <kometoume@gmail.com>
2025-07-02 13:04:07 +0900

    タイトル変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "八木美智惠 ポートフォリオ",
-  description: "ウェブ開発者八木美智惠のポートフォリオサイト",
+  title: "八木美智惠｜Webコーダー／フロントエンドエンジニア志望 ポートフォリオ",
+  description: "HTML/CSS/JavaScriptを中心にWebサイトを構築してきたWebコーダー。現在はReactやNext.jsを用いた開発にも携わり、フロントエンドエンジニアを目指しています。八木美智惠のポートフォリオサイトです。",
 };
 
 export default function RootLayout({
