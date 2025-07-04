e57b90643d9afc8e17af2f70f7d6ddadb13f73e0
Michie Yagi <kometoume@gmail.com>
2025-07-04 18:42:23 +0900

    Todoリスト修正01

--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -5,7 +5,7 @@ import TodoApp from '../../components/TodoApp';
 export default function TodosPage() {
   return (
     <main className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-4">
-      <h1 className="text-4xl font-extrabold text-gray-700 mb-3">Todoリスト</h1> {/* 日本語化 */}
+      <h1 className="text-4xl font-extrabold text-gray-700 mb-3">筋トレTodoリスト</h1>
       <TodoApp />
     </main>
   );
